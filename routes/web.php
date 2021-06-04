ce3d601e065a0554ba7589cef1d1a17ebe64388f
Eugen <gram25042002@gmail.com>
2021-06-04 17:43:32 +0300

    changed route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MoviesController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::get('/moviesp', [MoviesController::class, 'index'])->name('movies.index');
 
 Route::get('/popularm', function () {
     return view('pages.movies');
