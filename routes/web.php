3d13f0558919f14580d9279d7269706f151f8e19
Eugen <gram25042002@gmail.com>
2021-06-09 16:37:42 +0300

    added routes for tvshows

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,34 @@
 <?php
 
 use App\Http\Controllers\MoviesController;
+use App\Http\Controllers\TVController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Http;
 
+// trait defineCaregory
+// {
+//     public function getSetOfData()
+//     {
+//         $url = explode('/', url()->current());
+//         $category = end($url);
+//         $title = $category = match ($category) {
+//             'movies' => 'popular',
+//             'top-rated' => 'top_rated',
+//             'now-playing' => 'now_playing',
+//             default => $category
+//         };
+
+//         $title = ucwords(str_replace('_', ' ', $title));
+//         $movies = Http::withToken(config('services.tmdb.token'))
+//             ->get("https://api.themoviedb.org/3/movie/$category")
+//             ->json()['results'];
+
+//         //get genres
+//         $genres = Http::withToken(config('services.tmdb.token'))
+//             ->get("https://api.themoviedb.org/3/genre/movie/list")
+//             ->json()['genres'];
+//     }
+// }
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,9 +40,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/movies', [MoviesController::class, 'index'])->name('movies.index');
+Route::get('/movies/{category?}', [MoviesController::class, 'index'])->name('movies.index');
 Route::get('/movies/upcoming', [MoviesController::class, 'index'])->name('movies.index');
 Route::get('/movies/top-rated', [MoviesController::class, 'index'])->name('movies.index');
 Route::get('/movies/now-playing', [MoviesController::class, 'index'])->name('movies.index');
 
-// Route::resource('movies', MoviesController::class);//->names(['/upcoming' => 'movies.index']);
+Route::get('/tv', [TVController::class, 'index'])->name('tv.index');
+Route::get('/tv/airing-today', [TVController::class, 'index'])->name('tv.index');
+Route::get('/tv/airing-on-the-air', [TVController::class, 'index'])->name('tv.index');
+Route::get('/tv/top-rated', [TVController::class, 'index'])->name('tv.index');
